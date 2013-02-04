37adc80a51620937bafc3480ad56aefe25167e1a
Catchit <precious.bondwe@baobabhealth.org>
2013-02-04 11:28:55 +0200

    Script to migrate hiv staging encounters

---
+++ import_hiv_staging_encounters.sql
@@ -0,0 +1,5 @@
+# This procedure imports hiv reception encounters from intermediate tables to ART2 OpenMRS database
+# I decided to use default values / hardcoded values to reduce reads
+
+# encounter_type ====> 52
+
