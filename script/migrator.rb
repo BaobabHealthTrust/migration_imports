1142d0633ea3be902b0b6922f0bdb5e9e1376df7
timmtonga <timmtonga@gmail.com>
2013-07-24 14:15:55 +0200

    removed accidental limit that was pushed

--- script/migrator.rb
+++ script/migrator.rb
@@ -79,7 +79,7 @@ def start
   puts "Loaded concepts in #{elapsed}"
 
   #you can specify the number of patients to export by adding limit then number of patiets e.g limit 100 to the query below
-  patients = Patient.find_by_sql("Select * from #{Source_db}.patient where voided = 0 limit 2")
+  patients = Patient.find_by_sql("Select * from #{Source_db}.patient where voided = 0")
   patient_ids = patients.map{|p| p.patient_id}
   pat_ids =  [0] if patient_ids.blank?
   
