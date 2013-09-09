cda016904f5fe09109dfbb0989c5fa04c3e5baef
Deliwe <dbnkhoma@gmail.com>
2013-09-09 14:18:43 +0200

    updated the procedure_setup_partial.sh

--- procedure_setup_partial.sh
+++ procedure_setup_partial.sh
@@ -31,68 +31,34 @@ echo "start time : $now"
 
 echo "initializing $DATABASE (OpenMRS 1.7) destination database.............................."
 
-echo "DROP DATABASE $DATABASE;" | mysql --user=$USERNAME --password=$PASSWORD
-echo "CREATE DATABASE $DATABASE;" | mysql --user=$USERNAME --password=$PASSWORD
+echo "DROP DATABASE $DATABASE;" | mysql --user=$USERNAME --host=$HOST --password=$PASSWORD
+echo "CREATE DATABASE $DATABASE;" | mysql --user=$USERNAME --host=$HOST --password=$PASSWORD
 echo "loading concept_server_full_db"
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_1_7_2_concept_server_full_db.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE < db/openmrs_1_7_2_concept_server_full_db.sql
 echo "loading schema additions"
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/schema_bart2_additions.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_views_schema_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE < db/schema_bart2_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE < db/bart2_views_schema_additions.sql
 echo "loading defaults"
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/defaults.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE < db/defaults.sql
 echo "loading user schema modifications"
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/malawi_regions.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/mysql_functions.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/drug_ingredient.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/pharmacy.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/national_id.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/weight_for_heights.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/${SITE}.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/tasks.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/moh_regimens_only.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/retrospective_station_entries.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/create_dde_server_connection.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/migration_imports/create_weight_height_for_ages.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/migration_imports/insert_weight_for_ages.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/age_in_months.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/malawi_regions.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/mysql_functions.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/drug_ingredient.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/pharmacy.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/national_id.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/weight_for_heights.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/data/${SITE}/${SITE}.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/data/${SITE}/tasks.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/moh_regimens_only.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/retrospective_station_entries.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/create_dde_server_connection.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/migration_imports/create_weight_height_for_ages.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/migration_imports/insert_weight_for_ages.sql
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/age_in_months.sql
 
 echo "loading up-to-date concepts"
-mysql  --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_metadata_1_7.sql
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/temporary_tables.sql
-#-- FILES=schema/*.sql
-#-- for f in $FILES
-#-- do
-#-- 	echo "Installing $f..."
-#-- 	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
-#-- done
-
-#-- FILES=triggers/*.sql
-#-- for f in $FILES
-#-- do
-#-- 	echo "Installing $f..."
-#-- 	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
-#-- done
-
-#-- FILES=procedures/*.sql
-#-- for f in $FILES
-#-- do
-#-- 	echo "Installing $f..."
-#-- 	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
-#-- done
-
-#-- FILES=procedures/sub-procedures/inserts/*.sql
-#-- for f in $FILES
-#-- do
-#-- 	echo "Installing $f..."
-#-- 	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
-#-- done
-
-#-- FILES=procedures/sub-procedures/updates/*.sql
-#-- for f in $FILES
-#-- do
-#-- 	echo "Installing $f..."
-#-- 	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
-#-- done
+mysql  --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/openmrs_metadata_1_7.sql
+mysql  --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/temporary_tables.sql
 
 echo "loading import scripts.............................."
 
@@ -100,36 +66,13 @@ FILES=import_scripts/*.sql
 for f in $FILES
 do
 	echo "loading $f..."
-	mysql --user=$USERNAME --password=$PASSWORD $DATABASE < $f
+	mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < $f
 done
 
 echo "importing users......................................"
-mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE<<EOFMYSQL
 CALL proc_import_users;
 EOFMYSQL
-
-#echo "importing data......................................."
-#mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
-#CALL proc_import_patients;
-#EOFMYSQL
-
-#echo "creating dispensation, appointment and exit from HIV care encounters....."
-#mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
-#CALL proc_import_from_temp;
-#EOFMYSQL
-
-#echo "calculating adherence................................"
-
-#mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
-#CALL proc_update_obs_order_id;
-#EOFMYSQL
-
-#echo "deleting temp_encounter and temp_obs tables..........."
-
-#mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
-#DROP table temp_encounter;
-#DROP table temp_obs;
-#EOFMYSQL
 
 later=$(date +"%T")
 echo "start time : $now"
