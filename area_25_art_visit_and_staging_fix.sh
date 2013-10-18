da438b462c12c5339cf1688bc4f005028b511bee
Deliwe <dbnkhoma@gmail.com>
2013-10-18 17:40:42 +0200

    made some changes to area_25_art_visit_and_staging_fix script

--- area_25_art_visit_and_staging_fix.sh
+++ area_25_art_visit_and_staging_fix.sh
@@ -40,7 +40,7 @@ echo "Exporting to the intermediary storage"
  script/runner script/encounter_exporter.rb
 
 echo "Importing ART visit and HIV staging encounters"
-mysql  --user=$BART2_USERNAME --password=$BART2_PASSWORD --host=$BART2_HOST  $BART2_DATABASE < import_scripts/import_area_25_art_and_hiv_staging.sql
+mysql  --user=$BART2_USERNAME --password=$BART2_PASSWORD --host=$BART2_HOST  $BART2_DATABASE < import_scripts/import_art_and_hiv_staging_area_25_encounters.sql
 mysql  --user=$BART2_USERNAME --password=$BART2_PASSWORD --host=$BART2_HOST  $BART2_DATABASE < import_scripts/import_art_visit_encounters.sql
 mysql  --user=$BART2_USERNAME --password=$BART2_PASSWORD --host=$BART2_HOST  $BART2_DATABASE < import_scripts/import_hiv_staging_encounters.sql
 
