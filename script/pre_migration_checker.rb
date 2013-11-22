2ad3f96fce74b3e48240eb4f3235fae695a2f86e
timmtonga <timmtonga@gmail.com>
2013-11-22 09:46:41 +0200

    pre-migration checks

--- script/pre_migration_checker.rb
+++ script/pre_migration_checker.rb
@@ -3,9 +3,12 @@ Source_db = YAML.load(File.open(File.join(RAILS_ROOT, "config/database.yml"), "r
 def start
 
   encounters_without_creator = Encounter.find_by_sql("select * from #{Source_db}.encounter where creator = 0").length
+	minimum_patient_id = Patient.find_by_sql("Select MIN(patient_id) as patient_id from #{Source_db}.patient where voided = 0").patient_id
 	
 	puts ""
-  print "Encounters without creators".lju
+  print "Encounters without creators".ljust(30)
+  puts encounters_without_creator
+  
 
 
 end
