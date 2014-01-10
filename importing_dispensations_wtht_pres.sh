9caf55bc8a2dae1da8d4d9a457b6987628a10c06
Deliwe <dbnkhoma@gmail.com>
2014-01-10 08:42:35 +0200

    added scripts to import all dispensations encounters without prescriptions

---
+++ importing_dispensations_wtht_pres.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+usage(){
+  echo "Usage"
+  echo
+  echo "ENVIRONMENT should be: bart2"
+  echo "Available SITES:"
+  ls -1 db/data
+} 
+
+set -x # turns on stacktrace mode which gives useful debug information
+
+if [ ! -x config/database.yml ] ; then
+  cp config/database.yml.example config/database.yml
+fi
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['bart2']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['bart2']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['bart2']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['bart2']['host']"`
+
+now=$(date +"%F %T")
+echo "start time : $now"
+
+echo "loading import scripts.............................."
+
+mysql  --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < db/temporary_tables.sql
+
+FILES=import_scripts/*.sql
+for f in $FILES
+do
+	echo "loading $f..."
+	mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE < $f
+done
+
+echo "importing dispensations without prescriptions............................."
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST  $DATABASE<<EOFMYSQL
+CALL proc_import_patients_with_disp_wtht_pres;
+EOFMYSQL
+
+echo "creating dispensation, appointment and exit from HIV care encounters....."
+mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE<<EOFMYSQL
+CALL proc_import_from_temp;
+EOFMYSQL
+
+echo "fixing earliest_start_date"
+script/runner script/all_after_migration_scripts/fix_earliest_start_date.rb
+
+echo "deleting temp_encounter and temp_obs tables..........."
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
+  DROP table temp_encounter;
+  DROP table temp_obs;
+EOFMYSQL
+
+later=$(date +"%F %T")
+echo "start time : $now"
+echo "end time : $later"
+
+echo "done"
