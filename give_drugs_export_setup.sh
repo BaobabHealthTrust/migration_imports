f7fbe0bc9c29ddb5dbca1da08a1b2ad607ce11ac
Deliwe <dbnkhoma@gmail.com>
2014-04-23 09:02:07 +0200

    added the give_drugs_export script

---
+++ give_drugs_export_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+usage(){
+  echo "Usage: $0 ENVIRONMENT"
+  echo
+  echo "ENVIRONMENT should be: development|production"
+} 
+
+ENV=$1
+
+if [ -z "$ENV" ] ; then
+  usage
+  exit
+fi
+
+set -x # turns on stacktrace mode which gives useful debug information
+
+if [ ! -x config/database.yml ] ; then
+  cp config/database.yml.example config/database.yml
+fi
+
+USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
+PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
+DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
+HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
+
+now=$(date +"%F %T")
+echo "start time : $now"
+
+echo "Creating intermediary storage"
+ #rake db:drop
+ rake db:create
+ rake db:migrate
+
+echo "creating drug_map table"
+ script/runner script/create_drug_map.rb
+
+echo "Exporting to the intermediary storage"
+ script/runner script/give_drugs_orders_migrator.rb
+
+later=$(date +"%F %T")
+echo "start time : $now"
+echo "end time : $later"
