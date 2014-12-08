9a94cc4c8450ba8d49cc6c90ac3c0cd9f0b61153
Deliwe <dbnkhoma@gmail.com>
2014-12-08 15:48:28 +0200

    updated llh_art_start_date script

--- script/llh_art_start_date.rb
+++ script/llh_art_start_date.rb
@@ -3,7 +3,7 @@ CONN = ActiveRecord::Base.connection
 
 def start
   #get all patients with ART startdate linked to HIV reception encounter
-  art_start_date_obs = Encounter.find_by_sql("SELECT 
+   art_start_date_obs = Encounter.find_by_sql("SELECT 
                                                     e.patient_id, e.encounter_id,
                                                     e.encounter_datetime, e.date_created,
                                                     o.obs_id, o.concept_id,
@@ -19,40 +19,42 @@ def start
 
   #loop through each patient
   (art_start_date_obs || []).each do |patient|
-    puts "............................working on #{patient.patient_id}"
     #check if patient has first visit encounter during that visit
     existing_first_vis_enc = Encounter.find_by_sql("SELECT encounter_id, encounter_datetime, date_created
                                           FROM #{MY_ENV}.encounter
-                                          WHERE DATE(encounter_datetime) = '#{patient.encounter_datetime.strftime('%Y-%m-%d')}'
+                                          WHERE DATE(encounter_datetime) = '#{patient.encounter_datetime.to_date.strftime('%Y-%m-%d')}'
                                           AND patient_id = #{patient.patient_id}
                                           AND encounter_type = 1
                                           ORDER BY DATE(encounter_datetime) DESC
-                                          LIMIT 1").map(&:encounter_id)
-
+                                          LIMIT 1").map(&:encounter_id).first
+    
+     puts "............................working on #{patient.patient_id}"
      #if--yes--check if patient has art startdate obs
      if !existing_first_vis_enc.blank?
-       first_vis_enc_ART_startdate = Observation.find_by_sql("SELECT value_datetime
+       first_vis_enc_ART_startdate = Observation.find_by_sql("SELECT encounter_id
                                                               FROM #{MY_ENV}.obs
                                                               WHERE encounter_id = #{existing_first_vis_enc}
                                                               AND patient_id = #{patient.patient_id}
-                                                              AND concept_id = 143 ").map(&:value_datetime)
-       if !first_vis_enc_ART_startdate.blank?
-         #void hiv_reception ART startdate observations
+                                                              AND concept_id = 143 AND voided = 0").map(&:encounter_id)
+        if !first_vis_enc_ART_startdate.first.blank?
+
+	 #void hiv_reception ART startdate observations
          update_art_start_date_obs = "UPDATE #{MY_ENV}.obs
                                      SET voided = 1,
                                          voided_by = 1, 
                                          void_reason = 'created a HIV first visit encounter and linked this obs to it', 
-                                         date_voided = #{Date.today}
+                                         date_voided = '#{DateTime.now().strftime('%Y-%m-%d %H:%M:%S')}'
                                      WHERE obs_id = #{patient.obs_id}"
         CONN.execute update_art_start_date_obs
 
        else
+        
         #create observation and void the reception ART startdate obs
         obs = Observation.new()
         obs.patient_id = patient.patient_id
-        obs.encounter_id = first_vis_enc_ART_startdate
+        obs.encounter_id = existing_first_vis_enc
         obs.obs_datetime = patient.encounter_datetime
-        obs.date_created = Date.today
+        obs.date_created = DateTime.now().strftime('%Y-%m-%d %H:%M:%S')
         obs.concept_id =  patient.concept_id
         obs.value_datetime = patient.value_datetime
         obs.creator = patient.creator
@@ -64,7 +66,7 @@ def start
                                      SET voided = 1,
                                          voided_by = 1, 
                                          void_reason = 'created a HIV first visit encounter and linked this obs to it', 
-                                         date_voided = #{Date.today}
+                                         date_voided = '#{DateTime.now().strftime('%Y-%m-%d %H:%M:%S')}'
                                      WHERE obs_id = #{patient.obs_id}"
         CONN.execute update_art_start_date_obs
       
@@ -78,7 +80,7 @@ def start
        enc.patient_id = patient.patient_id
        enc.encounter_type = 1
        enc.encounter_datetime = patient.encounter_datetime
-       enc.date_created = Date.today
+       enc.date_created = DateTime.now().strftime('%Y-%m-%d %H:%M:%S')
        enc.provider_id = patient.provider_id
        enc.location_id = patient.location_id
        enc.save
@@ -91,7 +93,7 @@ def start
        obs.patient_id = patient.patient_id
        obs.encounter_id = new_enc_id
        obs.obs_datetime = patient.encounter_datetime
-       obs.date_created = Date.today
+       obs.date_created = DateTime.now().strftime('%Y-%m-%d %H:%M:%S')
        obs.concept_id =  patient.concept_id
        obs.value_datetime = patient.value_datetime
        obs.creator = patient.creator
@@ -103,7 +105,7 @@ def start
                                      SET voided = 1,
                                          voided_by = 1, 
                                          void_reason = 'created a HIV first visit encounter and linked this obs to it', 
-                                         date_voided = #{Date.today}
+                                         date_voided = '#{DateTime.now().strftime('%Y-%m-%d %H:%M:%S')}'
                                      WHERE obs_id = #{patient.obs_id}"
         CONN.execute update_art_start_date_obs
              
