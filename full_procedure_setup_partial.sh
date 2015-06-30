82e86f3e50e7a56845ea3986f93ca4ad5ba2baef
Deliwe Nkhoma <dbnkhom@gmail.com>
2015-06-30 18:47:33 +0200

    made changes to some of the scripts that run after the migration

--- full_procedure_setup_partial.sh
+++ full_procedure_setup_partial.sh
@@ -66,32 +66,32 @@ mysql --user=$USERNAME --password=$PASSWORD --host=$HOST $DATABASE<<EOFMYSQL
 CALL proc_update_obs_order_id;
 EOFMYSQL
 
-#echo "fixing retired drugs"
-#script/runner script/all_after_migration_scripts/fix_program_locations.rb
+echo "fixing retired drugs"
+script/runner script/all_after_migration_scripts/fix_program_locations.rb
 
-#echo "fixing equivalent daily dose"
-#script/runner script/all_after_migration_scripts/fix_for_equivalent_daily_dose.rb
+echo "fixing equivalent daily dose"
+script/runner script/all_after_migration_scripts/fix_for_equivalent_daily_dose_llh.rb
 
-#echo "adding the hanging pills"
-#script/runner script/all_after_migration_scripts/include_hanging_pills_to_drug_orders.rb
+echo "adding the hanging pills"
+script/runner script/all_after_migration_scripts/include_hanging_pills_to_drug_orders_llh.rb
 
-#echo "recalculating adherence"
-#script/runner script/all_after_migration_scripts/recalculate_adherence.rb
+echo "recalculating adherence"
+script/runner script/all_after_migration_scripts/recalculate_adherence.rb
 
-#echo "creating OPD program"
-#script/runner script/all_after_migration_scripts/creating_patient_opd_program.rb
+echo "creating OPD program"
+script/runner script/all_after_migration_scripts/creating_patient_opd_program.rb
 
-#echo "fixing earliest_start_date"
-#script/runner script/all_after_migration_scripts/fix_earliest_start_date.rb
+echo "fixing earliest_start_date"
+script/runner script/all_after_migration_scripts/fix_earliest_start_date.rb
 
-#echo "fixing arv numbers"
-#script/runner script/arv_format_fix.rb 
+echo "fixing arv numbers"
+script/runner script/arv_format_fix.rb 
 
-#echo "fixing all patients on HIV program and on ARVs without any dispensing encounter"
-#script/runner script/all_after_migration_scripts/patients_on_hiv_prog_without_disp_enc_fix.rb 
+echo "fixing all patients on HIV program and on ARVs without any dispensing encounter"
+script/runner script/all_after_migration_scripts/patients_on_hiv_prog_without_disp_enc_fix.rb 
 
-#echo "fixing all patients with Pre-ART state but are not on HIV program"
-#script/runner script/all_after_migration_scripts/pre_art_hiv_program_fix.rb
+echo "fixing all patients with Pre-ART state but are not on HIV program"
+script/runner script/all_after_migration_scripts/pre_art_hiv_program_fix.rb
 
 #echo "deleting temp_encounter and temp_obs tables..........."
 #mysql --user=$USERNAME --password=$PASSWORD $DATABASE<<EOFMYSQL
